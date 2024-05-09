ce62217be6e9add63343095a117534fc3e63d448
AlanDAmario <vega93@live.it>
2024-05-09 19:09:37 +0200

    [UPD] milestone 1 without comment

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,2 +1,20 @@
 'use strict';
-.innerHTML
+//CREIAMO UN EVENTLISTENER PER I BOTTONI
+const clickButton = document.querySelectorAll('.numbers-selector');
+let result = document.getElementById('result');
+let addNumb = '';
+let printNumb = '';
+addNumbers();
+
+
+
+function addNumbers() {
+    for (let i = 0; i < clickButton.length; i++) {
+        // console.log(clickButton[i].innerHTML);
+        clickButton[i].addEventListener('click', function () {
+            addNumb = clickButton[i].innerHTML;
+            printNumb = printNumb + addNumb;
+            result.innerHTML = printNumb;
+        });
+    }
+}
